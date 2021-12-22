ee22c062359a4daf6837f058d384e489727ff1d1
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-12-22 23:49:47 +0600

    login and Register validation completed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 
 			<div class="wrap-breadcrumb">
 				<ul>
-					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><a href="/" class="link">home</a></li>
 					<li class="item-link"><span>Register</span></li>
 				</ul>
 			</div>
@@ -14,18 +14,20 @@
 					<div class=" main-content-area">
 						<div class="wrap-login-item ">
 							<div class="register-form form-item ">
-								<form class="form-stl" action="#" name="frm-login" method="get" >
+                                <x-jet-validation-errors class="mb-4" />
+								<form class="form-stl" action="{{route('register')}}" name="frm-login" method="POST" >
+                                    @csrf
 									<fieldset class="wrap-title">
 										<h3 class="form-title">Create an account</h3>
 										<h4 class="form-subtitle">Personal infomation</h4>
 									</fieldset>									
 									<fieldset class="wrap-input">
 										<label for="frm-reg-lname">Name*</label>
-										<input type="text" id="frm-reg-lname" name="reg-lname" placeholder="Last name*">
+										<input type="text" id="frm-reg-lname" name="name" placeholder="Your name*" :value="name" required autofocus autocomplete="name">
 									</fieldset>
 									<fieldset class="wrap-input">
 										<label for="frm-reg-email">Email Address*</label>
-										<input type="email" id="frm-reg-email" name="reg-email" placeholder="Email address">
+										<input type="email" id="frm-reg-email" name="email" placeholder="Email address" :value="email">
 									</fieldset>
 									 
 									<fieldset class="wrap-title">
@@ -33,13 +35,14 @@
 									</fieldset>
 									<fieldset class="wrap-input item-width-in-half left-item ">
 										<label for="frm-reg-pass">Password *</label>
-										<input type="text" id="frm-reg-pass" name="reg-pass" placeholder="Password">
+										<input type="password" id="frm-reg-pass" name="password" placeholder="Password" required autocomplete="new-password">
 									</fieldset>
 									<fieldset class="wrap-input item-width-in-half ">
 										<label for="frm-reg-cfpass">Confirm Password *</label>
-										<input type="text" id="frm-reg-cfpass" name="reg-cfpass" placeholder="Confirm Password">
+										<input type="password" id="frm-reg-cfpass" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
 									</fieldset>
-									<input type="submit" class="btn btn-sign" value="Register" name="register">
+									<!-- <input type="submit" class="btn btn-sign" value="Register" name="register"> -->
+                                    <button type = "submit" class ="btn btn-primary" value = "Register">Submit</button>
 								</form>
 							</div>											
 						</div>
