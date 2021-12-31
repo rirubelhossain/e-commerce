f20c221b421ce7e75fc4f016dc45c469b5557be9
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-12-31 08:10:39 +0600

    Admin Edit Category

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Livewire\User\UserDashboardComponent ;
 use App\Http\Livewire\Admin\AdminDashboardComponent ;
 use App\Http\Livewire\Admin\AdminCategoryComponent ;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent ;
+use App\Http\Livewire\Admin\AdminEditCategoryComponent ;
 use App\Http\Livewire\SearchComponent ;
 use App\Http\Livewire\CategoryComponent ;
 
@@ -44,6 +45,7 @@ Route::middleware(['auth:sanctum', 'verified' , 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
+    Route::get('/admin/category/edit/{category_slug}', AdminEditCategoryComponent::class)->name('admin.editcategory');
 }); 
 
 
