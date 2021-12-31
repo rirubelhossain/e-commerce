641b9dccc913485ad345eb7ea3ec0da44ea4a6b3
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-12-31 07:41:38 +0600

    Admin Category

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\DetailsComponent ;
 use App\Http\Livewire\User\UserDashboardComponent ;
 use App\Http\Livewire\Admin\AdminDashboardComponent ;
 use App\Http\Livewire\Admin\AdminCategoryComponent ;
+use App\Http\Livewire\Admin\AdminAddCategoryComponent ;
 use App\Http\Livewire\SearchComponent ;
 use App\Http\Livewire\CategoryComponent ;
 
@@ -42,6 +43,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function(){
 Route::middleware(['auth:sanctum', 'verified' , 'authadmin'])->group(function(){
     Route::get('/admin/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
     Route::get('/admin/categories', AdminCategoryComponent::class)->name('admin.categories');
+    Route::get('/admin/category/add', AdminAddCategoryComponent::class)->name('admin.addcategory');
 }); 
 
 
