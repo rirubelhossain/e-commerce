31bb728597e54641a09d21ea2efc48d79dd4b81f
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2022-01-01 21:09:12 +0600

    Admin Delete category

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -8,6 +8,13 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component
 {   
     use WithPagination ;
+
+    public function deleteCategory($id){
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message','Category has been Deleted successfully');
+    }
+
     public function render()
     {   
         $categories = Category::paginate(5);
--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -22,6 +22,12 @@
                         </div>
                     </div>
                     <div class="panel-body">
+                        @if(Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{Session::get('message')}}
+                        </div>
+
+                        @endif
                         <table class="table table-stripted">
                             <thead>
                                 <tr>
@@ -40,6 +46,7 @@
 
                                         <td>
                                             <a href="{{route('admin.editcategory',['category_slug'=>$category->slug])}}" ><i class="fa fa-edit fa-2x"></i> </a>
+                                            <a href="" wire:click.prevent="deleteCategory({{$category->id}})" style="margin-left:10px"><i class="fa fa-times fa-2x text-danger"></i> </a>
                                         </td>
 
                                     </tr>
